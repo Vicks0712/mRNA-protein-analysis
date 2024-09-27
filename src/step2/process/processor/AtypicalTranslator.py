3f9ee060d7eb7749bfed97ff841229f42852812e
vicks0712 <vt@xrf.ai>
2024-09-27 22:17:54 +0100

    feat(modeling): Clustering example for proteins created.

--- src/step2/process/processor/AtypicalTranslator.py
+++ src/step2/process/processor/AtypicalTranslator.py
@@ -1,10 +1,10 @@
 from src.step2.process.mRNAProcessor import ARNmProcessor
+from Bio.Seq import Seq
 
 
 class AtypicalTranslator(ARNmProcessor):
     def process_sequence(self, seq_record):
         # Ejemplo de traducción atípica, este método podría aplicar otro procesamiento.
-        from Bio.Seq import Seq
         # A modo de ejemplo, puede realizar una "traducción" personalizada
         # basada en algún otro patrón o regla
         atypical_seq = Seq(seq_record.seq).reverse_complement().translate(to_stop=True)
